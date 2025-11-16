30db5a64e626b38dace36cb5cdf2311c59fd7df6
vijaysaiboya864 <vijaysaiboya864@gmail.com>
2025-11-16 22:44:17 +0530

    Designed dashboard screen with options to search movies,view details and view saved movies by list

---
+++ app/src/main/java/vijaysaiboya/movietrackerapp/madproject/DashboardActivity.kt
@@ -0,0 +1,13 @@
+package vijaysaiboya.movietrackerapp.madproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
